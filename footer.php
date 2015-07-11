d947ed8f03430756cdd26d373fdff44c4251db51
chriszarate <chris@zarate.org>
2015-07-11 14:45:32 -0400

    Update to new physical address.

--- footer.php
+++ footer.php
@@ -1,35 +1,35 @@
 <!-- footer.php -->
 
-<div id="footer" class="clearfix">	
+<div id="footer" class="clearfix">
 
 <div id="footer_inner">
 
 	<?php
-	
+
 	// are we using the page footer widget?
 	if ( !dynamic_sidebar( 'cp-license-8' ) ) {
 
 		// compat with wplicense plugin
 		if ( function_exists( 'isLicensed' ) AND isLicensed() ) {
-		
+
 			// show the license (buggy, use wp_footer() instead)
 			//licenseHtml( $display = true );
-			
+
 		} else {
-			
+
 			// show copyright
 			?>
-			
+
 			<p><strong>Modern Language Association</strong><br>
-			26 Broadway, 3rd floor<br>
-			New York, NY 10004</p>
-			
-			<?php 
-					
+			85 Broad Street, suite 500<br>
+			New York, NY 10004-2434</p>
+
+			<?php
+
 		}
-		
+
 	}
-	
+
 	?>
 
 </div><!-- /footer_inner -->
